0230a64e29f788f1c4c3f0f0ed67335158647f1b
Matthew Todd <matthew.todd@gmail.com>
2009-08-07 12:58:47 +0300

    Documentation.

--- bundle/bootstrap.rb
+++ bundle/bootstrap.rb
@@ -5,5 +5,12 @@ unless ENV.has_key?('SERVER')
   ENV['OPENID_SERVER'] = 'http://10.0.1.2:4001'
 end
 
+# I decided, on-balance, not to use a validation token here, since I'd like to
+# pre-configure all the recipes for the node in the chef-server (from home)
+# before running the bootstrapping process. (I'd like the node's configuration
+# to include the chef::client recipe, but that would result in a race between
+# 2 chef clients, bootstrap & runit, for the dpkg lock.) So, as a simple
+# not-too-painful workaround, I'll just need to manually validate the node's
+# registration in the web ui after bootstrapping.
 system 'mount /media/cdrom0'
 system 'chef-solo --config /cdrom/bundle/solo.rb --json-attributes /cdrom/bundle/solo.json'
--- bundle/solo.rb
+++ bundle/solo.rb
@@ -1,9 +1,10 @@
+cookbook_path    '/cdrom/bundle/cookbooks'
+file_cache_path  '/tmp/chef-solo'
+
+# Give solo the server URLs so that they'll propagate to the client.
 registration_url ENV['SERVER']
 openid_url       ENV['OPENID_SERVER'] || ENV['SERVER']
 template_url     ENV['SERVER']
 remotefile_url   ENV['SERVER']
 search_url       ENV['SERVER']
 role_url         ENV['SERVER']
-
-cookbook_path    '/cdrom/bundle/cookbooks'
-file_cache_path  '/tmp/chef-solo'
